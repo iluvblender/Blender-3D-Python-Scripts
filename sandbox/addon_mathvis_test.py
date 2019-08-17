059f26a6aa790f9d92ea460dfad719fd03432181
Satish Goda <satishgoda@live.com>
2019-08-17 00:21:00 -0700

    Create addon_mathvis_test.py

---
+++ sandbox/addon_mathvis_test.py
@@ -0,0 +1,27 @@
+"""
+# Usage
+
+mod = bpy.data.texts['mathviz1.py'].as_module()
+exec(mod.code)
+loc = sum((bb0, bb4, bb7, bb3), Vector((0, 0, 0))) * 0.25
+bpy.ops.object.empty_add(location=loc)
+"""
+
+import bpy
+from mathutils import Vector
+
+
+code = """
+ao = bpy.context.active_object
+
+mtx = ao.matrix_world
+
+bbpts = map(Vector, ao.bound_box)
+
+for index, point in enumerate(bbpts):
+    exec("bb{0} = ao.matrix_world @ point".format(index), 
+          globals(),
+    )
+
+del(point)
+"""
