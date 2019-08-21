698913a0017b0810425cdc3255116edde09e73a8
Satish Goda <satishgoda@live.com>
2019-08-21 12:40:38 -0700

    Update operator_layout_meshes.py
    Added a menu for choosing the axes of alignment. Also, Alignment is relative to the active object.

--- toolbox/layout/operators/operator_layout_meshes.py
+++ toolbox/layout/operators/operator_layout_meshes.py
@@ -1,12 +1,23 @@
 import bpy
-from bpy.props import FloatProperty
+from bpy.props import FloatProperty, EnumProperty
 
 
 class LayoutMeshesOperator(bpy.types.Operator):
-    """Lays out selected Mesh objects on the X-axis"""
+    """Lays out selected Mesh objects on the a selected axis"""
     bl_idname = "object.layout_meshes"
-    bl_label = "Layout Mesh objects on X-axis"
+    bl_label = "Layout Mesh objects on X/Y/Z axis"
     bl_options = {'REGISTER', 'UNDO'}
+    
+    axis: EnumProperty(
+        name="Axis of Alignment",
+        description="Axis along which alignment operation will take place",
+        default="X",
+        items=(
+        ('X', 'X-Axis', 'X Axis'),
+        ('Y', 'Y-Axis', 'Y Axis'),
+        ('Z', 'Z-Axis', 'Z Axis')
+        )
+    )
     
     gap: FloatProperty(
         name='Gap',
@@ -28,12 +39,37 @@ class LayoutMeshesOperator(bpy.types.Operator):
         selected_objects = context.selected_objects
         index = selected_objects.index(ao)
         selected_objects.pop(index)
-
-        offset = ao.bound_box[4][0] + self.gap
+        
+        axes_index = {
+            'X': 0,
+            'Y': 1,
+            'Z': 2
+        }
+        
+        bbox_axes_index = {
+            'min': {
+                'X': 0,
+                'Y': 4,
+                'Z': 7
+            },
+            'max': {
+                'X': 4,
+                'Y': 7,
+                'Z': 6
+            }
+        }
+        
+        offset = ao.location[axes_index[self.axis]] + ao.bound_box[bbox_axes_index['max'][self.axis]][axes_index[self.axis]] + self.gap
 
         for obj in selected_objects:
-            obj.location.x = offset + abs(obj.bound_box[0][0])
-            offset = obj.location.x + abs(obj.bound_box[4][0]) + self.gap
+            other_axes = axes_index.copy()
+            other_axes.pop(self.axis)
+
+            for axis in other_axes.values():
+                obj.location[axis] = ao.location[axis]
+
+            obj.location[axes_index[self.axis]] = offset + abs(obj.bound_box[bbox_axes_index['min'][self.axis]][axes_index[self.axis]])
+            offset = obj.location[axes_index[self.axis]] + abs(obj.bound_box[bbox_axes_index['max'][self.axis]][axes_index[self.axis]]) + self.gap
 
         return {'FINISHED'}
 
