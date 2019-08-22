2a55f0694fccb6d4b9f65036a0a1bc5de8c7cedc
Satish Goda <satishgoda@live.com>
2019-08-21 22:11:53 -0700

    Update properties_workspace.py

--- ui/panel/custom/properties_workspace.py
+++ ui/panel/custom/properties_workspace.py
@@ -6,11 +6,14 @@ class WORKSPACE_PT_operator(WorkSpaceButtonsPanel, Panel):
         layout.operator("screen.repeat_last")
         layout.operator("screen.repeat_history")
 
+
 class WORKSPACE_PT_test_operator(WorkSpaceButtonsPanel, Panel):
     bl_label = "Test Operator Development"
     
     def draw(self, context):
         layout = self.layout
+        layout.operator("object.layout_meshes")
+        layout.alert = True
         layout.operator_enum("object.layout_meshes", 'axis')        
         
 classes = (
