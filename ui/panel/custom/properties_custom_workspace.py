db9a3dba79a5903acad996a6b0981e6a911e10c7
Satish Goda <satishgoda@live.com>
2019-08-26 12:05:51 -0700

    Update and rename properties_workspace.py to properties_custom_workspace.py

--- ui/panel/custom/properties_custom_workspace.py
+++ ui/panel/custom/properties_custom_workspace.py
@@ -1,3 +1,12 @@
+import bpy
+from bpy.types import (
+    Panel,
+)
+
+from bl_ui.properties_workspace import WorkSpaceButtonsPanel
+
+
+
 class WORKSPACE_PT_operator(WorkSpaceButtonsPanel, Panel):
     bl_label = "Operators"
     
@@ -14,11 +23,17 @@ class WORKSPACE_PT_test_operator(WorkSpaceButtonsPanel, Panel):
         layout = self.layout
         layout.operator("object.layout_meshes")
         layout.alert = True
-        layout.operator_enum("object.layout_meshes", 'axis')        
-        
+        layout.operator_enum("object.layout_meshes", 'axis')
+        layout.operator("object.bake_vertex_normal_to_color")
+
+
 classes = (
-    ....,
     WORKSPACE_PT_operator,
     WORKSPACE_PT_test_operator,
-    ....,
 )
+
+
+if __name__ == "__main__":  # only for live edit.
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
