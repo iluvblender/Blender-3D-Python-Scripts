08556a78e81834b11fd1c6793069bbc718dde888
Satish Goda <satishgoda@live.com>
2020-04-01 17:54:20 -0700

    Create scene_render_settings.py

---
+++ sandbox/scene_render_settings.py
@@ -0,0 +1,6 @@
+C.scene.render.filepath = "//playblast/playblast.v001."
+C.scene.render.image_settings.file_format = 'FFMPEG'
+C.scene.render.ffmpeg.format = 'MPEG4'
+C.scene.render.ffmpeg.codec = 'H264'
+C.scene.render.ffmpeg.constant_rate_factor = 'LOSSLES'
+C.scene.render.ffmpeg.ffmpeg_preset = 'BEST'
